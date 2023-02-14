7913fc9cbda78b341772908b51ec49e5558358ce
Jean Santos <dev.jean.ap@gmail.com>
2023-02-14 10:37:29 -0300

    fix: correção de bug, onde não estava sendo impresso o diagnóstico do usuário

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 function init() {
   const button = document.querySelector(".button");
+  const diagnostico = document.querySelector(".diagnostico");
 
-  const calculaImc = () => {
-    const inputPeso = document.querySelector(".peso");
-    const inputAltura = document.querySelector(".altura");
-    inputPeso = +inputPeso.value; //converte string em number
-    inputAltura = +inputAltura.value; //converte string em number
-    const imc = inputPeso / inputAltura ** 2;
-    return imc;
+  const captaValores = () => {
+    const inputNome = document.querySelector("#nome");
+    const inputPeso = document.querySelector("#peso").value;
+    const inputAltura = document.querySelector("#altura").value;
+    calculaImc(inputNome, inputPeso, inputAltura);
   };
 
-  const imprimeDiagnostico = () => {
-    const inputNome = document.querySelector(".nome");
-    const diagnostico = document.querySelector(".diagnostico");
-    const imc = calculaImc();
-    diagnostico.innerHTML += `${inputNome.value}, seu IMC é ${imc}`;
+  const calculaImc = (inputNome, inputPeso, inputAltura) => {
+    inputPeso = +inputPeso; //converte string em number
+    inputAltura = +inputAltura; //converte string em number
+    const imc = inputPeso / inputAltura ** 2;
+    imprimeDiagnostico(inputNome, imc);
+  };
+
+  const classificacaoImc = () => {
+    switch (imc) {
+      case ap:
+        break;
+      case pn:
+        break;
+      case sp:
+        break;
+      case og1:
+        break;
+      case og2:
+        break;
+      case og3:
+        break;
+    }
+  };
+
+  const imprimeDiagnostico = (inputNome, imc) => {
+    diagnostico.innerHTML += `<div>${inputNome.value}`;
+    diagnostico.innerHTML += `<div>Seu IMC é ${imc}</div>`;
+    diagnostico.innerHTML += `<div>Você está classificado em `;
   };
 
   if (button) {
     button.addEventListener("click", (e) => {
       e.preventDefault();
-      imprimeDiagnostico();
-      console.log("Tudo ok");
+      captaValores();
     });
   }
 }
